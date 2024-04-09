24afb82e8560f036291112d1162f7493e9d6ce36
CSylvie <sylvie.castet@gmail.com>
2024-04-09 11:07:41 +0200

    initial commit

---
+++ src/Container/Container.js
@@ -0,0 +1,11 @@
+import "./Container.css" ; 
+
+function Container () {
+    return (
+        <div className="gamesContainer">
+            TEST POUR VOIR
+        </div>
+    )
+}
+
+export default Container; 
